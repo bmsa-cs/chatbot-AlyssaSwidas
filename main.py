c6b412e479fe1bb43939c876277cb56ea4ff8da7
Alyssa Swidas <aswidas24@biomedscienceacademy.org>
2021-02-18 19:31:00 +0000

    I finished my chatbot

--- main.py
+++ main.py
@@ -59,6 +59,28 @@ elif m == "ok":
 else:
     print ("It's okay everyone has off days")
  
+a = int(input("How many siblings do you have? "))
+if s == 5:
+    print(" I have five siblings to")
+elif s == 0:
+    print (" So your an only child thats cool ")
+else:
+    print (" I have multiple siblings too ")
+
+c = input("What is your favorite color?")
+if c == "blue":
+    print("That is also my favorite color")
+else:
+    print("That is a pretty color")
+  
+s = input("Do you play any sports")
+if s == " yes":
+    print (" Thats cool ")
+elif s == "no":
+    print ("Thats okay you don't have to") 
+else:
+    print (" I play softball ")
+
 
 
 if __name__ == "__main__":
